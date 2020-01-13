63ff63ca22e3efcf81125da6b9efb45b0a364cb3
ianguita <iam_98@hotmail.es>
2020-01-13 17:52:14 +0100

    ToDo basic

--- app/src/main/java/com/usefulapps/allinone/MainActivity.java
+++ app/src/main/java/com/usefulapps/allinone/MainActivity.java
@@ -9,18 +9,29 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
     RelativeLayout calculadora;
+    RelativeLayout todo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         calculadora = findViewById(R.id.calculadora);
+        todo = findViewById(R.id.todo);
+
         calculadora.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Calculadora.class));
             }
         });
-    }
 
+        todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ToDo.class));
+            }
+        });
+    }
 }
