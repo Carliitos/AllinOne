50d69a59049ea445e470ba6f52454c42bcf85ebe
Carliitos <colado.molina.carlos@alumnat.copernic.cat>
2020-01-13 16:12:17 +0100

    Pushing everything

---
+++ app/src/main/java/com/usefulapps/allinone/Calculadora.java
@@ -0,0 +1,175 @@
+package com.usefulapps.allinone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class Calculadora extends AppCompatActivity {
+    RelativeLayout btn1;
+    RelativeLayout btn2;
+    RelativeLayout btn3;
+    RelativeLayout btn4;
+    RelativeLayout btn5;
+    RelativeLayout btn6;
+    RelativeLayout btn7;
+    RelativeLayout btn8;
+    RelativeLayout btn9;
+    RelativeLayout btn0;
+    RelativeLayout btndiv;
+    RelativeLayout btnmult;
+    RelativeLayout btneq;
+    RelativeLayout btnres;
+    TextView restext;
+    RelativeLayout btnsum;
+    RelativeLayout btnresta;
+    Integer xifra1;
+    Integer xifra2;
+    String operador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btn0 = findViewById(R.id.btn0);
+        btndiv = findViewById(R.id.btndivision);
+        btnmult = findViewById(R.id.btnmult);
+        btnres = findViewById(R.id.btnresult);
+        restext = findViewById(R.id.restext);
+        btnsum = findViewById(R.id.btnsuma);
+        btnresta = findViewById(R.id.btnresta);
+
+        btndiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addsimbolo("/");
+            }
+        });
+        btnmult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addsimbolo("*");
+            }
+        });
+        btnresta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addsimbolo("-");
+            }
+        });
+        btnres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addsimbolo("=");
+            }
+        });
+        btnsum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addsimbolo("+");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(2);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(3);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(4);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(5);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(6);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(7);
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(8);
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(9);
+            }
+        });
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addnumber(0);
+            }
+        });
+
+    }
+    void addnumber(Integer num){
+        restext.setText(restext.getText()+num.toString());
+    }
+    void addsimbolo(String simbolo){
+        if(!simbolo.equals("=")){
+            operador = simbolo;
+            if(xifra1==null){
+                xifra1 = Integer.parseInt(restext.getText().toString());
+                restext.setText("");}
+        }else{
+
+                // else if(xifra2 == null){
+            //    xifra2 = Integer.parseInt(restext.getText().toString());
+            //}
+            if(xifra2 == null){
+                xifra2 = Integer.parseInt(restext.getText().toString());
+                if(operador.equals("+")){
+                    Log.d("MyApp","Xifra 1"+xifra1);
+                    Log.d("MyApp","Xifra 2"+xifra2);
+                    Integer result = xifra1+xifra2;
+                    restext.setText(result+"");
+                    xifra1 = null;
+                    xifra2 = null;
+                }
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/usefulapps/allinone/MainActivity.java
+++ app/src/main/java/com/usefulapps/allinone/MainActivity.java
@@ -2,13 +2,25 @@ package com.usefulapps.allinone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    RelativeLayout calculadora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calculadora = findViewById(R.id.calculadora);
+        calculadora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Calculadora.class));
+            }
+        });
     }
+
 }
