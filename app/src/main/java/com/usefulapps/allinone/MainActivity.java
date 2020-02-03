c6e7143d69c632180da9937120a762cd7a4643e2
Carliitos <colado.molina.carlos@alumnat.copernic.cat>
2020-02-03 18:10:15 +0100

    Register

--- app/src/main/java/com/usefulapps/allinone/MainActivity.java
+++ app/src/main/java/com/usefulapps/allinone/MainActivity.java
@@ -1,67 +1,85 @@
 package com.usefulapps.allinone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    RelativeLayout calculadora;
-    RelativeLayout todo;
-    RelativeLayout password;
-    RelativeLayout reminder;
-    Button loggin;
+    // Initialize Firebase Auth
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    EditText email;
+    EditText password;
+    EditText passwordRepeated;
+    Button login;
     Button register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        calculadora = findViewById(R.id.calculadora);
-        todo = findViewById(R.id.todo);
-        reminder = findViewById(R.id.reminder);
-        loggin = findViewById(R.id.login);
-        register = findViewById(R.id.register);
+        setContentView(R.layout.activity_login2);
+
+        email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-        calculadora.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Calculadora.class));
-            }
-        });
-        loggin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Login.class));
-            }
-        });
+        passwordRepeated = findViewById(R.id.passwordrepeated);
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Register.class));
             }
         });
-        password.setOnClickListener(new View.OnClickListener() {
+
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, PasswordGenerator.class));
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startActivity(new Intent(MainActivity.this, Menu.class));
+                                    //updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Hola", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+
+                                // ...
+                            }
+                        });
             }
         });
-        todo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ToDo.class));
-            }
-        });
-        reminder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Reminder.class));
-            }
-        });
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
 
-
+    }
 }
